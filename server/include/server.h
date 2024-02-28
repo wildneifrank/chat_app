7d04f4299d02a7f087d485ec5d11a394c47e0663
wsilva6 <wildneif2@gmail.com>
2024-02-28 15:46:08 -0300

    removed auth functions and created Login, Register and, also, added auth_ as a private property

--- server/include/server.h
+++ server/include/server.h
@@ -7,8 +7,7 @@
 #include <vector>
 #include <nlohmann/json.hpp>
 #include <httplib.h>
-#include "user.h"
-#include "message.h"
+#include "auth.h"
 
 namespace server {
     class Server{
@@ -16,16 +15,14 @@ namespace server {
         Server();
         ~Server();
         void InitializationServer();
-        void PostMessages();
+        void Login();
+        void Register();
         void GetData();
-        int NameAlreadyExists(const std::string& user_name);
         
         private:
+        std::shared_ptr<Auth> auth_;
         std::shared_ptr<httplib::Server> server_;
-        std::vector<User> users_;
-        std::vector<Message> messages_;
         std::shared_ptr<nlohmann::json> json_data_;
-        std::shared_ptr<int> current_id_;
     };
 } // namespace server
 
