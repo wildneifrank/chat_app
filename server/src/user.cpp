b9a059c8277aaabd00147b99b40fdda13e9c76ba
wsilva6 <wildneif2@gmail.com>
2024-02-29 15:38:02 -0300

    implemented all user header functions

--- server/src/user.cpp
+++ server/src/user.cpp
@@ -4,10 +4,13 @@
 #include "../include/user.h"
 
 namespace server{
-    User::User(const std::string& name, const int& id):
+    User::User(const std::string& name, const int& id, const std::string& email, const std::string& token, const std::string& encrypted_password):
                 name_(std::make_shared<std::string>(name)),
-                id_(std::make_shared<int>(id)){}
-    User::~User(){}
+                id_(std::make_shared<int>(id)),
+                email_(std::make_shared<std::string>(email)),
+                encrypted_password_(std::make_shared<std::string>(encrypted_password)),
+                token_(std::make_shared<std::string>(token))
+                {}
 
     std::shared_ptr<std::string> User::GetName() const{
         return name_;
@@ -17,5 +20,20 @@ namespace server{
         return id_;
     }
 
+    std::shared_ptr<std:string> User::GetToken() const{
+        return token_;
+    }
+
+    void User::SetToken(const std::string& token){
+        token_ = std::make_shared<std::string>(token);
+    }
+
+    std::shared_ptr<std::string> GetPassword() const{
+        return encrypted_password_;
+    }
+
+    std::shared_ptr<std::string> GetEmail() const{
+        return email_;
+    }
 
 } // namespace server
